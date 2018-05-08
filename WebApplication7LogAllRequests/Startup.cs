18e0860cd11969a4968be0c32391ef9fc2658443
Jim Raden <radenj@ccf.org>
2018-05-08 17:24:12 -0400

    Log all incoming requests

--- WebApplication7LogAllRequests/Startup.cs
+++ WebApplication7LogAllRequests/Startup.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using WebApplication7LogAllRequests.Utility;
 
 namespace WebApplication7LogAllRequests
 {
@@ -18,6 +18,7 @@ namespace WebApplication7LogAllRequests
             Configuration = configuration;
         }
 
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -27,6 +28,7 @@ namespace WebApplication7LogAllRequests
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // ReSharper disable once UnusedMember.Global
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,7 +36,44 @@ namespace WebApplication7LogAllRequests
                 app.UseDeveloperExceptionPage();
             }
 
+            // Log all requests
+            app.Use(LogAllRequestsMiddleware(env));
+
             app.UseMvc();
+        }
+
+        private static Func<HttpContext, Func<Task>, Task> LogAllRequestsMiddleware(IHostingEnvironment env)
+        {
+            return async (context, next) =>
+            {
+                var traceIdentifier = context.TraceIdentifier;
+                var requestHeader = RequestHelper.GetRequestMethodAndUrl(context.Request);
+                var headers = RequestHelper.GetAllHeaders(context.Request);
+                var body = RequestHelper.ReadBodyIntoString(context.Request);
+
+                var directory = Path.Combine(env.ContentRootPath, "Logs");
+                var filename = traceIdentifier.Replace(':', '_');
+                const string extension = "http";
+                //if (context.Request.ContentType?.Contains("xml") == true)
+                //{
+                //    extension = "xml";
+                //}
+                //else if (context.Request.ContentType?.Contains("json") == true)
+                //{
+                //    extension = "json";
+                //}
+                //else
+                //{
+                //    extension = "txt";
+                //}
+                var filepath = $@"{directory}\{filename}.{extension}";
+                File.WriteAllText(filepath, $"{requestHeader}\n");
+                File.AppendAllLines(filepath, headers.Select(kv => $"{kv.Item1}: {kv.Item2}"));
+                File.AppendAllText(filepath, "\n");
+                File.AppendAllText(filepath, body);
+
+                await next.Invoke();
+            };
         }
     }
 }
---
+++ WebApplication7LogAllRequests/Utility/RequestHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplication7LogAllRequests.Utility
+{
+    public static class RequestHelper
+    {
+        public static string ReadBodyIntoString(HttpRequest request)
+        {
+            using (var reader = new StreamReader(request.Body))
+            {
+                string body = reader.ReadToEnd();
+                return body;
+            }
+        }
+
+        public static IEnumerable<(string, string)> GetAllHeaders(HttpRequest request)
+        {
+            return request.Headers
+                .Select(requestHeader => (requestHeader.Key, requestHeader.Value.ToString()));
+        }
+
+        public static string GetRequestMethodAndUrl(HttpRequest request)
+        {
+            return $"{request.Method} {request.GetUri()} {request.Protocol}";
+        }
+    }
+}
